a6c3f3f51f0b4fad28b377477b3a0b31449c2271
Ruttika <ruttikajadhav@gmail.com>
2024-12-16 22:54:37 +0530

    Basic Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Product from "./Pages/Product";
+import Pricing from "./Pages/Pricing";
+import HomePage from "./Pages/HomePage";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppRoutes = () => {
+	const location = useLocation();
+	console.log(location.pathname); // Now this will work
+	return (
+		<Routes>
+			<Route path='/' element={<HomePage />} />
+			<Route path='product' element={<Product />} />
+			<Route path='pricing' element={<Pricing />} />
+		</Routes>
+	);
+};
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+export const App = () => {
+	return (
+		<React.StrictMode>
+			<BrowserRouter>
+				<AppRoutes />
+			</BrowserRouter>
+		</React.StrictMode>
+	);
+};
 
-export default App
+export default App;
