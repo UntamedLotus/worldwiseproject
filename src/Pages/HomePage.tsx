10af990654aa7fb2a42b88b85abe5b5936ec25e9
Ruttika <ruttikajadhav@gmail.com>
2024-12-19 20:59:41 +0530

    New files added

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,9 +2,20 @@ import React from "react";
 
 const HomePage = () => {
 	return (
-		<div>
-			<h1>HomePage</h1>
-		</div>
+		<main>
+			<section>
+				<h1>
+					You travel the world.
+					<br />
+					WorldWise keeps track of your adventures.
+				</h1>
+				<h2>
+					A world map that tracks your footsteps into every city you
+					can think of. Never forget your wonderful experiences, and
+					show your friends how you have wandered the world.
+				</h2>
+			</section>
+		</main>
 	);
 };
 
